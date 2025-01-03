3784a5a22d4b00ae12906fd1cac29108a582d9fb
candzon <mf123cw@gmail.com>
2025-01-03 11:05:02 +0700

    Update tampil_penilaian_vendor.php

--- application/views/admin/tampil_penilaian_vendor.php
+++ application/views/admin/tampil_penilaian_vendor.php
@@ -2,18 +2,18 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Penilaian Vendor</h1>
     </div>
-    
+
     <!-- Flash Message -->
     <?php if ($this->session->flashdata('success')): ?>
-    <div class="alert alert-success">
-        <?= $this->session->flashdata('success'); ?>
-    </div>
+        <div class="alert alert-success">
+            <?= $this->session->flashdata('success'); ?>
+        </div>
     <?php endif; ?>
 
     <?php if ($this->session->flashdata('error')): ?>
-    <div class="alert alert-danger">
-        <?= $this->session->flashdata('error'); ?>
-    </div>
+        <div class="alert alert-danger">
+            <?= $this->session->flashdata('error'); ?>
+        </div>
     <?php endif; ?>
 
     <div class="panel panel-default">
@@ -22,6 +22,41 @@
             <div class="d-flex justify-content-center">
                 <form id="form-penilaian" action="<?= base_url('data_vendor_ga/tambah_penilaian_vendor_aksi') ?>" method="post" enctype="multipart/form-data">
                     <div class="col">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <h6>Keterangan Penilaian:</h6>
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Nilai</th>
+                                            <th>Keterangan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Sangat Tidak Puas</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Tidak Puas</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Cukup Puas</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Puas</td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>Sangat Puas</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                         <div class="row mb-3">
                             <label for="select-vendor" class="col-form-label">Nama Vendor</label>
                             <div class="col">
@@ -37,39 +72,39 @@
                             <h6>Tingkat Kepuasan Evaluasi</h6>
                         </div>
                         <?php if ($data != null): ?>
-                        <?php foreach ($data as $item): ?>
-                        <div class="row mt-4">
-                            <table class="table table-striped-columns">
-                                <tr>
-                                    <h5><?= $item['kategori']->kode_kategori; ?> <?= $item['kategori']->keterangan; ?></h5>
-                                    <th>No</th>
-                                    <th>Aspek Penilaian</th>
-                                    <th>Nilai</th>
-                                </tr>
-                                <tbody>
-                                    <?php $detail_no = 1;
-                                    foreach ($item['details'] as $detail):
-                                    ?>
-                                    <tr>
-                                        <td><?= $detail_no; ?></td>
-                                        <td><?= $detail->aspek_penilaian; ?></td>
-                                        <td>
-                                            <select name="nilai[<?= $detail->id_aspek_penilaian; ?>]" class="form-control" required>
-                                                <option value="">Pilih Nilai</option>
-                                                <option value="1">1</option>
-                                                <option value="2">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                                <option value="5">5</option>
-                                            </select>
-                                        </td>
-                                    </tr>
-                                    <?php $detail_no++;
-								endforeach; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                        <?php endforeach; ?>
+                            <?php foreach ($data as $item): ?>
+                                <div class="row mt-4">
+                                    <table class="table table-striped-columns">
+                                        <tr>
+                                            <h5><?= $item['kategori']->kode_kategori; ?> <?= $item['kategori']->keterangan; ?></h5>
+                                            <th>No</th>
+                                            <th>Aspek Penilaian</th>
+                                            <th>Nilai</th>
+                                        </tr>
+                                        <tbody>
+                                            <?php $detail_no = 1;
+                                            foreach ($item['details'] as $detail):
+                                            ?>
+                                                <tr>
+                                                    <td><?= $detail_no; ?></td>
+                                                    <td><?= $detail->aspek_penilaian; ?></td>
+                                                    <td>
+                                                        <select name="nilai[<?= $detail->id_aspek_penilaian; ?>]" class="form-control" required>
+                                                            <option value="">Pilih Nilai</option>
+                                                            <option value="1">1</option>
+                                                            <option value="2">2</option>
+                                                            <option value="3">3</option>
+                                                            <option value="4">4</option>
+                                                            <option value="5">5</option>
+                                                        </select>
+                                                    </td>
+                                                </tr>
+                                            <?php $detail_no++;
+                                            endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            <?php endforeach; ?>
                         <?php endif; ?>
                         <div class="row mt-4 d-flex justify-content-between">
                             <!-- <textarea name="komentar" id="komentar" placeholder="komentar"></textarea> -->
@@ -87,7 +122,7 @@
 
 <!-- JavaScript SweetAlert -->
 <script>
-    document.getElementById('btn-submit').addEventListener('click', function (e) {
+    document.getElementById('btn-submit').addEventListener('click', function(e) {
         Swal.fire({
             title: 'Konfirmasi Pengajuan',
             text: 'Apakah Anda yakin ingin menyelesaikan evaluasi ini?',
